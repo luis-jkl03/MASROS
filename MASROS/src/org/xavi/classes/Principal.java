3377b6a5be2dd51f261ebe194af1b9a167a197c6
sagit <sagit@DESKTOP-ALH5QG4>
2019-05-11 23:44:11 -0500

    Added Hola mundo

--- MASROS/src/org/xavi/classes/Principal.java
+++ MASROS/src/org/xavi/classes/Principal.java
@@ -3,7 +3,9 @@ package org.xavi.classes;
 public class Principal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		for(int i = 0; i < 10; i++)
+			System.out.println("Hola mundo");
 
 	}
 
