edfd1bd6fcbdbca229ec5f4c87ea516cb3f59917
sagit <sagit@DESKTOP-ALH5QG4>
2019-05-12 00:19:28 -0500

    Add client props and init interfaz

---
+++ MASROS/src/org/maven/settings/CreateInterfaz.java
@@ -0,0 +1,26 @@
+package org.maven.settings;
+
+public class CreateInterfaz {
+
+	private int cont;
+	private String message;
+	
+	
+	
+	
+	public int getCont() {
+		return cont;
+	}
+	public void setCont(int cont) {
+		this.cont = cont;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	
+	
+	
+}
